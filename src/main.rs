905ed451d4d009a559512573c3030a622d7961ae
SecondarySkyler <skylerhunter6@gmail.com>
2025-01-26 15:36:13 +0100

    added drones/clients/servers channels for scl constructor

--- src/main.rs
+++ src/main.rs
@@ -118,7 +118,7 @@ fn main() {
         create_channels(&drone, &client, &server).collect();
 
     // spawn drones
-    for d in drone {
+    for d in &drone {
         let nbrs: HashMap<NodeId, Sender<Packet>> = d
             .connected_node_ids
             .iter()
@@ -136,7 +136,19 @@ fn main() {
     }
 
     // TODO spawn client/server + start scl
-    let mut scl = SimulationController::new(0, HashMap::new(), HashMap::new(), HashMap::new());
+    let mut scl_drones_channels: HashMap<NodeId, (Sender<DroneCommand>, Receiver<DroneEvent>)> = HashMap::new();
+    let mut scl_clients_channels: HashMap<NodeId, (Sender<ClientCommand>, Receiver<ClientEvent>)> = HashMap::new();
+    let mut scl_servers_channels: HashMap<NodeId, (Sender<ServerCommand>, Receiver<ServerEvent>)> = HashMap::new();
+    for d in &drone {
+        scl_drones_channels.insert(d.id, (scl_commands[&d.id].0.clone(), scl_events[&d.id].1.clone()));
+    }
+    for c in &client {
+        scl_clients_channels.insert(c.id, (scl_client_commands[&c.id].0.clone(), scl_client_events[&c.id].1.clone()));
+    }
+    for s in &server {
+        scl_servers_channels.insert(s.id, (scl_server_commands[&s.id].0.clone(), scl_server_events[&s.id].1.clone()));
+    }
+    let mut scl = SimulationController::new(0, scl_drones_channels, scl_clients_channels, scl_servers_channels);
     // std::thread::spawn(move || scl.run()); // Apparently this cant be done because "EventLoop must be created on the main thread"
     scl.run();
 }
