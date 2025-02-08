f4fb531e33457de3a49619083b965fe926500224
simone3c <simone.dagnino@studenti.unitn.it>
2025-02-08 16:18:27 +0100

    faire drones tests

--- src/test.rs
+++ src/test.rs
@@ -457,3 +457,164 @@ fn test_drdrone(){
     );
 }
 
+#[test]
+fn test_bettercalldrone(){
+    let (_dcmds, devents, stctrl, _stevents, smctrl, _smevents, cctrl, cevents) =
+            instanciate_testing_topology::<BetterCallDrone>();
+    generic_full_file_request(
+        devents,
+        stctrl,
+        smctrl,
+        cevents,
+        cctrl,
+        "./public/file.html".to_owned(),
+        |r: TextMediaResponse| {
+            assert!(r.get_media_files().is_empty());
+            assert!(r.get_html_file().1 == fs::read("./public/file.html").unwrap());
+        },
+    );
+}
+
+#[test]
+fn test_getdroned(){
+    let (_dcmds, devents, stctrl, _stevents, smctrl, _smevents, cctrl, cevents) =
+            instanciate_testing_topology::<GetDroned>();
+    generic_full_file_request(
+        devents,
+        stctrl,
+        smctrl,
+        cevents,
+        cctrl,
+        "./public/file.html".to_owned(),
+        |r: TextMediaResponse| {
+            assert!(r.get_media_files().is_empty());
+            assert!(r.get_html_file().1 == fs::read("./public/file.html").unwrap());
+        },
+    );
+}
+
+#[test]
+fn test_rustinpeace(){
+    let (_dcmds, devents, stctrl, _stevents, smctrl, _smevents, cctrl, cevents) =
+            instanciate_testing_topology::<NoSoundDroneRIP>();
+    generic_full_file_request(
+        devents,
+        stctrl,
+        smctrl,
+        cevents,
+        cctrl,
+        "./public/file.html".to_owned(),
+        |r: TextMediaResponse| {
+            assert!(r.get_media_files().is_empty());
+            assert!(r.get_html_file().1 == fs::read("./public/file.html").unwrap());
+        },
+    );
+}
+
+#[test]
+fn test_rollingdrone(){
+    let (_dcmds, devents, stctrl, _stevents, smctrl, _smevents, cctrl, cevents) =
+            instanciate_testing_topology::<RollingDrone>();
+    generic_full_file_request(
+        devents,
+        stctrl,
+        smctrl,
+        cevents,
+        cctrl,
+        "./public/file.html".to_owned(),
+        |r: TextMediaResponse| {
+            assert!(r.get_media_files().is_empty());
+            assert!(r.get_html_file().1 == fs::read("./public/file.html").unwrap());
+        },
+    );
+}
+
+#[test]
+fn test_rustdoit(){
+    let (_dcmds, devents, stctrl, _stevents, smctrl, _smevents, cctrl, cevents) =
+            instanciate_testing_topology::<RustDoIt>();
+    generic_full_file_request(
+        devents,
+        stctrl,
+        smctrl,
+        cevents,
+        cctrl,
+        "./public/file.html".to_owned(),
+        |r: TextMediaResponse| {
+            assert!(r.get_media_files().is_empty());
+            assert!(r.get_html_file().1 == fs::read("./public/file.html").unwrap());
+        },
+    );
+}
+
+#[test]
+fn test_rustroveri(){
+    let (_dcmds, devents, stctrl, _stevents, smctrl, _smevents, cctrl, cevents) =
+            instanciate_testing_topology::<RustRoveri>();
+    generic_full_file_request(
+        devents,
+        stctrl,
+        smctrl,
+        cevents,
+        cctrl,
+        "./public/file.html".to_owned(),
+        |r: TextMediaResponse| {
+            assert!(r.get_media_files().is_empty());
+            assert!(r.get_html_file().1 == fs::read("./public/file.html").unwrap());
+        },
+    );
+}
+
+#[test]
+fn test_rustafarian(){
+    let (_dcmds, devents, stctrl, _stevents, smctrl, _smevents, cctrl, cevents) =
+            instanciate_testing_topology::<RustafarianDrone>();
+    generic_full_file_request(
+        devents,
+        stctrl,
+        smctrl,
+        cevents,
+        cctrl,
+        "./public/file.html".to_owned(),
+        |r: TextMediaResponse| {
+            assert!(r.get_media_files().is_empty());
+            assert!(r.get_html_file().1 == fs::read("./public/file.html").unwrap());
+        },
+    );
+}
+
+#[test]
+fn test_rusteze(){
+    let (_dcmds, devents, stctrl, _stevents, smctrl, _smevents, cctrl, cevents) =
+            instanciate_testing_topology::<RustezeDrone>();
+    generic_full_file_request(
+        devents,
+        stctrl,
+        smctrl,
+        cevents,
+        cctrl,
+        "./public/file.html".to_owned(),
+        |r: TextMediaResponse| {
+            assert!(r.get_media_files().is_empty());
+            assert!(r.get_html_file().1 == fs::read("./public/file.html").unwrap());
+        },
+    );
+}
+
+#[test]
+fn test_rustydrone(){
+    let (_dcmds, devents, stctrl, _stevents, smctrl, _smevents, cctrl, cevents) =
+            instanciate_testing_topology::<RustyDrone>();
+    generic_full_file_request(
+        devents,
+        stctrl,
+        smctrl,
+        cevents,
+        cctrl,
+        "./public/file.html".to_owned(),
+        |r: TextMediaResponse| {
+            assert!(r.get_media_files().is_empty());
+            assert!(r.get_html_file().1 == fs::read("./public/file.html").unwrap());
+        },
+    );
+}
