4bd9833800132429abc065c608cf72c4c459f2d2
simone3c <simone.dagnino@studenti.unitn.it>
2025-02-08 12:27:20 +0100

    butterfly + test

--- src/test.rs
+++ src/test.rs
@@ -227,3 +227,18 @@ fn star(){
     assert!(check_topology_constraints(&drone, &client, &server));
 
 }
+
+#[test]
+fn butterfly(){
+    let config_data: String =
+        fs::read_to_string("config/butterfly.toml").expect("Unable to read config file");
+    // having our structs implement the Deserialize trait allows us to use the toml::from_str function to deserialize the config file into each of them
+    let Config {
+        drone,
+        client,
+        server,
+    }: Config = toml::from_str(&config_data).expect("Unable to parse TOML");
+
+    assert!(check_topology_constraints(&drone, &client, &server));
+
+}
